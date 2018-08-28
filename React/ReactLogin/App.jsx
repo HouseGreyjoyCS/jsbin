17bafa05354f07b91d45a8e22f92f61e3dc16782
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-08-28 14:48:49 -0700

    Further connected functionality of login and signup components to App.jsx

--- React/ReactLogin/App.jsx
+++ React/ReactLogin/App.jsx
@@ -7,37 +7,48 @@ class App extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            password:""
+            username:'',
+            password:'',
         }
-        this.passwordCheck = this.passwordCheck.bind(this);
+        // this.passwordCheck = this.passwordCheck.bind(this);
+        this.logIn = this.logIn.bind(this);
         this.signUp = this.signUp.bind(this);
-        this.handleGet = this.handleGet.bind(this);
+        // this.handleGet = this.handleGet.bind(this);
     }
-    passwordCheck() {
-        fetch('http://localhost:3000/bin/:name')
-        .then((res =>{
-            res.json()
-        } ))
-        .then(res => {
-            console.log()
-            this.setState({ password: res})
-        })
-        .catch(err => console.log('Error grabbing bins ', err))
+    // passwordCheck() {
+    //     fetch('http://localhost:3000/bin/:name')
+    //     .then((res =>{
+    //         res.json()
+    //     } ))
+    //     .then(res => {
+    //         console.log()
+    //         this.setState({ password: res})
+    //     })
+    //     .catch(err => console.log('Error grabbing bins ', err))
+    // }
+    logIn(username, password) {
+        // post request to DB goes here - TO DO
+        console.log("username from App.jsx logIn", username);
+        console.log("password from App.jsx logIn", password);
+        //window.location.href = 'http://localhost:3000/admin/'
     }
-    signUp() {
-        // post request to DB goes here
+    signUp(username, password) {
+        // post request to DB goes here - TO DO
+        console.log("username from App.jsx signUp", username);
+        console.log("password from App.jsx signUp", password);
+        //window.location.href = 'http://localhost:3000/admin/'
     }
-    handleGet(e){
-        this.setState({password: e.target.value})
-    }
+    // handleGet(e){
+    //     this.setState({password: e.target.value})
+    // }
     
     render () {
        return (
            <div className="loginWrapper">
            <Router>
                <div>
-                   <Route path="/login" render={() => <Login />} />
-                   <Route path="/signup" render={() => <Signup />} />
+                   <Route path="/login" render={() => <Login logIn={this.logIn}/>} />
+                   <Route path="/signup" render={() => <Signup signUp={this.signUp}/>} />
                 </div>
            </Router>
            </div>
--- React/ReactLogin/Login.jsx
+++ React/ReactLogin/Login.jsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, hashHistory, Link } from "react-router-dom";
 
-const Login = (props) =>{
-    console.log(props)
-   return (
-     
-       <div>
-           <h3>Log In</h3> 
-           <input type="text" placeholder="Username" required/>
-           <br />
-           <input type="text" placeholder="Password" required/>
-           <br />
-           <button class="button" onClick={props.signupSubmit} value={props.Signup}>Log In</button>
-           <br />
-           <br />
-           <p>Don't have an account? <Link to='/signup'>Sign Up</Link></p>
-       </div>
-       
-        // <div>
-        //  <h3>Password</h3>
-        //  <input type="text" />
-        //  <button class="button" onClick={props.passwordCheck} value={props.Password}>Password</button>
-        // </div>
-    );
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {username_val: '', password_val: ''};
+
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeUsername(event) {
+        this.setState({username_val: event.target.value});
+    }
+
+    handleChangePassword(event) {
+        this.setState({password_val: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault(); // need to do this so page won't reload
+        this.props.logIn(this.state.username_val, this.state.password_val);
+        this.setState({username_val: '', password_val: ''});
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Log In</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Username" value={this.state.username_val} onChange={this.handleChangeUsername} required/>
+                    <br />
+                    <input type="text" placeholder="Password" value={this.state.password_val} onChange={this.handleChangePassword} required/>
+                    <br />
+                    <br />
+                    <input type="submit" value="Sign Up" />
+                </form>
+                <p>Don't have an account? <Link to='/signup'>Sign Up</Link></p>
+            </div>
+        );
+    }
+
 }
 export default Login;
