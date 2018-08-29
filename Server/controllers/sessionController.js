131732b3503c4047f06931e0cb40f2351c6e791c
Colin Roemer <colin.roemer@gmail.com>
2018-08-29 15:40:59 -0700

    updated login routes to pass back user bins to render on admin page load - commented out though since we are going to now do a get request on admin page load

--- Server/controllers/sessionController.js
+++ Server/controllers/sessionController.js
@@ -6,7 +6,13 @@ const db = require('../db.js');
 const sessionController = {};
 
 sessionController.startSession = (req, res) => {
-  res.status(200).send()
+  // res.locals.data.accessed_bins = '123';
+  //   console.log(res.locals);
+    res.status(200).send()
+    // .json({
+    //   accessed_bins: res.locals.data.accessed_bins,
+    //   created_bins: res.locals.data.created_bins
+    // })
   // res.sendFile(path.resolve(__dirname, '../../build/admin/index.html'));
 }
 
