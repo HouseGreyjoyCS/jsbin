8386271920d9cfdb545b3b3e99a6f6b9b6f3bbdd
Colin Roemer <colin.roemer@gmail.com>
2018-08-28 17:32:29 -0700

    fixed db insert error on session insert

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -17,7 +17,10 @@ userController.createUser = (req, res, next) => {
   })
   .then(data => { 
     res.locals.data = data;
-    db.one('INSERT INTO sessions(session_id) VALUES($1)', [data._id]);
+    db.query('INSERT INTO sessions(session_id) VALUES($1)', [data._id])
+    .catch(err => {
+      console.log('hi im an error', err);
+    })
     console.log(res.locals);
     next();
   })
